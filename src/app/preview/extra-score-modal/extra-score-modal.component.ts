b921c9de163688ea78d75f38df9fe62eb9967d0e
varun <varun.eilane@prodevbase.com>
2021-05-30 14:42:04 +0530

    added the latest code

--- src/app/preview/extra-score-modal/extra-score-modal.component.ts
+++ src/app/preview/extra-score-modal/extra-score-modal.component.ts
@@ -6,14 +6,24 @@ import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
   styleUrls: ['./extra-score-modal.component.scss'],
 })
 export class ExtraScoreModalComponent implements OnInit {
-
+  scrore:number;
+  scoreType:string;
+  modalData;
   constructor(
     private dialogRef: MatDialogRef<ExtraScoreModalComponent>,
-    @Inject(MAT_DIALOG_DATA) data: string
-  ) { }
-
-  ngOnInit() {}
+    @Inject(MAT_DIALOG_DATA) matDetails: string
+  ) { this.modalData=matDetails; }
+  ngOnInit() {
+   
+  }
   closeModal() {
     this.dialogRef.close(false)
+  }
+  saveData(){
+    const scoreData={
+      score:this.scrore,
+      scoreType:this.scoreType||null
+    }
+    this.dialogRef.close(scoreData);
   }
 }
--- src/app/preview/preview.page.ts
+++ src/app/preview/preview.page.ts
@@ -4,6 +4,7 @@ import { ExtraScoreModalComponent } from './extra-score-modal/extra-score-modal.
 import {
   MatDialog, MatDialogRef
 } from "@angular/material/dialog";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.page.html',
@@ -29,10 +30,11 @@ export class PreviewPage implements OnInit {
     name: '',
     balls: 0
   }
-  selectedInningType: string='first';
+  selectedInningType: string = 'first';
   constructor(
     private loginService: LoginService,
-    private matDialog: MatDialog
+    private matDialog: MatDialog,
+    private router:Router
   ) {
     this.matchDetails = JSON.parse(this.loginService.getMatchDetails());
   }
@@ -98,12 +100,52 @@ export class PreviewPage implements OnInit {
       })
     }
   }
-  noballScoreUpdate() {
-    this.extraScore = this.extraScore + 1;
+  wideBallScoreUpdate(score){
+    this.extraScore = this.extraScore + Number(score);
     this.scoreValue = null;
     this.sumOfBatingTeamScore();
     // for bowler details update
-    this.activeBowlerDetails.runs = Number(this.activeBowlerDetails.runs) + Number(1);
+    this.activeBowlerDetails.runs = Number(this.activeBowlerDetails.runs) + Number(score);
+  }
+  noballScoreUpdate(score,type) {
+    // if we click on no ball (from bats man)
+   
+      if (this.matchDetailsForBoard['activeBating1'].isSelected) {
+        this.batingPlayers.forEach(bating => {
+          if (bating.isSelected) {
+            if(type &&type==='fromBat'){
+            bating.score = bating.score + Number(score);
+            this.matchDetailsForBoard['activeBating1'].score = this.matchDetailsForBoard['activeBating1'].score + Number(score);
+            }
+            this.matchDetailsForBoard['activeBating1'].ballsFaced = this.matchDetailsForBoard['activeBating1'].ballsFaced + 1;
+          }
+        })
+      } else if (this.matchDetailsForBoard['activeBating2'].isSelected) {
+        this.batingPlayers.forEach(bating => {
+          if (bating.isSelected) {
+            if(type &&type==='fromBat'){
+            bating.score = bating.score + Number(score);
+            this.matchDetailsForBoard['activeBating2'].score = this.matchDetailsForBoard['activeBating2'].score + Number(score);
+
+            }
+            this.matchDetailsForBoard['activeBating2'].ballsFaced = this.matchDetailsForBoard['activeBating2'].ballsFaced + 1;
+          }
+        })
+      }
+    
+    
+   
+    if(type &&type==='fromBat'){
+      this.extraScore = this.extraScore + Number(1);
+     // for bowler details update
+     this.activeBowlerDetails.runs = Number(this.activeBowlerDetails.runs) + Number(score+1);
+    }else{
+      this.extraScore = this.extraScore + Number(1+score);
+      this.activeBowlerDetails.runs = Number(this.activeBowlerDetails.runs) + Number(1);
+    }
+    this.scoreValue = null;
+    this.sumOfBatingTeamScore();
+   
   }
   scoreUpdate(value) {
     if (!this.matchDetailsForBoard['extraScore'] || this.matchDetailsForBoard['extraScore'] === 'nb') {
@@ -129,14 +171,18 @@ export class PreviewPage implements OnInit {
       if (this.matchDetailsForBoard['activeBating1'].isSelected) {
         this.batingPlayers.forEach(bating => {
           if (bating.isSelected) {
+            if(this.matchDetailsForBoard['extraScore'] !== 'by' && this.matchDetailsForBoard['extraScore'] !=='lb'){
             bating.score = bating.score + Number(value);
+            }
             this.matchDetailsForBoard['activeBating1'].ballsFaced = this.matchDetailsForBoard['activeBating1'].ballsFaced + 1;
           }
         })
       } else if (this.matchDetailsForBoard['activeBating2'].isSelected) {
         this.batingPlayers.forEach(bating => {
           if (bating.isSelected) {
+            if(this.matchDetailsForBoard['extraScore'] !== 'by' && this.matchDetailsForBoard['extraScore'] !=='lb'){
             bating.score = bating.score + Number(value)
+            }
             this.matchDetailsForBoard['activeBating2'].ballsFaced = this.matchDetailsForBoard['activeBating2'].ballsFaced + 1;
           }
         })
@@ -152,7 +198,7 @@ export class PreviewPage implements OnInit {
         this.matchDetailsForBoard['activeBatsman'] = 'first';
       }
     }
-    this.matchDetailsForBoard['extraScore'] = null;
+    
     // update the overs
     let balls = Number(this.matchDetailsForBoard['ballDetails'].balls);
     if (this.matchDetailsForBoard['ballDetails'].balls < 5) {
@@ -163,7 +209,9 @@ export class PreviewPage implements OnInit {
     }
     this.sumOfBatingTeamScore();
     // for bowler details update
+    if(this.matchDetailsForBoard['extraScore'] !== 'by' && this.matchDetailsForBoard['extraScore'] !=='lb'){
     this.activeBowlerDetails.runs = Number(this.activeBowlerDetails.runs) + Number(value);
+    }
     // this.activeBowlerDetails.runs=this.activeBowlerDetails['balls']+1;
     if (this.activeBowlerDetails.balls < 5) {
       this.activeBowlerDetails.balls = balls + 1;
@@ -171,6 +219,7 @@ export class PreviewPage implements OnInit {
       this.activeBowlerDetails.overs = this.activeBowlerDetails.overs + 1;
       this.activeBowlerDetails.balls = 0;
     }
+    this.matchDetailsForBoard['extraScore'] = null;
   }
   wicketClick() {
     this.activeBowlerDetails.wickets = Number(this.activeBowlerDetails.wickets) + 1;
@@ -205,9 +254,9 @@ export class PreviewPage implements OnInit {
     }
 
   }
-  extraScoreClicked() {
+  extraScoreClicked(type) {
     this.scoreValue = null;
-    this.openModal();
+    this.openModal(type);
   }
   activeBatingPlayerChanged() {
     // this.batingPlayers.forEach(bating => {
@@ -384,40 +433,40 @@ export class PreviewPage implements OnInit {
   }
   inningsClicked(type) {
     this.selectedInningType = type;
-   this.matchDetailsForBoard['batingScoreSum']=0;
-   this.matchDetailsForBoard['ballDetails'].overs=0;
-    this.matchDetailsForBoard['ballDetails'].balls=0
+    this.matchDetailsForBoard['batingScoreSum'] = 0;
+    this.matchDetailsForBoard['ballDetails'].overs = 0;
+    this.matchDetailsForBoard['ballDetails'].balls = 0
     // for players data
-    this.matchDetailsForBoard['activeBating1'].score=0;
-     this.matchDetailsForBoard['activeBating1'].ballsFaced=0;
-     this.matchDetailsForBoard['activeBating2'].score=0;
-     this.matchDetailsForBoard['activeBating2'].ballsFaced=0;
-     // for bowler details
-    this.activeBowlerDetails.overs=0;
-     this.activeBowlerDetails.balls=0;
-     this.activeBowlerDetails.runs=0;
-     this.activeBowlerDetails.wickets=0;
-    if(this.selectedInningType==='second'){
+    this.matchDetailsForBoard['activeBating1'].score = 0;
+    this.matchDetailsForBoard['activeBating1'].ballsFaced = 0;
+    this.matchDetailsForBoard['activeBating2'].score = 0;
+    this.matchDetailsForBoard['activeBating2'].ballsFaced = 0;
+    // for bowler details
+    this.activeBowlerDetails.overs = 0;
+    this.activeBowlerDetails.balls = 0;
+    this.activeBowlerDetails.runs = 0;
+    this.activeBowlerDetails.wickets = 0;
+    if (this.selectedInningType === 'second') {
       this.matchDetailsForBoard['team2ShortName'] = this.matchDetailsForBoard['team2Name'] + '(Batting)';
       this.matchDetailsForBoard['team1shortname'] = this.matchDetailsForBoard['team1Name'] + '(Bowling)';
       if (this.teamDetails && this.teamDetails.length) {
         this.teamDetails.map(team => {
-          if (team.SelectedTo && (team.SelectedTo === 'bat' || team.SelectedTo === 'BAT' 
-          || team.SelectedTo === "Bat")) {
+          if (team.SelectedTo && (team.SelectedTo === 'bat' || team.SelectedTo === 'BAT'
+            || team.SelectedTo === "Bat")) {
             this.matchDetails.bowlingTeam = team.team;
           }
           if (team.SelectedTo && (team.SelectedTo === 'BOWL' || team.SelectedTo === 'bowl')) {
-            this.matchDetails.battingTeam= team.team;
+            this.matchDetails.battingTeam = team.team;
           }
         })
       }
-    }else{
+    } else {
       this.matchDetailsForBoard['team2ShortName'] = this.matchDetailsForBoard['team2Name'] + '(Bowling)';
       this.matchDetailsForBoard['team1shortname'] = this.matchDetailsForBoard['team1Name'] + '(Batting)';
       if (this.teamDetails && this.teamDetails.length) {
         this.teamDetails.map(team => {
-          if (team.SelectedTo && (team.SelectedTo === 'bat' || team.SelectedTo === 'BAT' 
-          || team.SelectedTo === "Bat")) {
+          if (team.SelectedTo && (team.SelectedTo === 'bat' || team.SelectedTo === 'BAT'
+            || team.SelectedTo === "Bat")) {
             this.matchDetails.battingTeam = team.team;
           }
           if (team.SelectedTo && (team.SelectedTo === 'BOWL' || team.SelectedTo === 'bowl')) {
@@ -428,18 +477,49 @@ export class PreviewPage implements OnInit {
     }
     // changing the bating team name
     // this.matchDetails.battingTeam=
-    
+
   }
-  openModal(){
-    this.matDialogRef = 
-    this.matDialog.open(ExtraScoreModalComponent, {
-      data: { name:'' },
-      disableClose: true,
-      panelClass:'col-sm-5'
-    });
+  openModal(type) {
+    const modalData={
+      title:''
+    }
+    if(type==='by'){
+      modalData.title="Bye";
+    }else if(type==='lb'){
+      modalData.title="Leg Bye";
+    }else if(type==='wd'){
+      modalData.title='Wide Ball';
+    }else if(type==='nb'){
+      modalData.title='No Ball';
+    }
+    this.matDialogRef =
+      this.matDialog.open(ExtraScoreModalComponent, {
+        data: modalData,
+        disableClose: true,
+        panelClass: 'col-sm-5'
+      });
     this.matDialogRef.afterClosed().subscribe(res => {
-      if ((res == true)) {
+      if (res) {
+        if (res.score) {
+          const scoreValue=res;
+          const noOfScore=scoreValue.score||0;
+          switch (type) {
+            case 'by':
+            case 'lb':
+              this.scoreUpdate(noOfScore);
+              break;
+            case 'wd':
+              this.wideBallScoreUpdate(noOfScore+1);
+              break;
+              case 'nb':
+                this.noballScoreUpdate(noOfScore,scoreValue.scoreType);
+          }
+        }
       }
     });
+  }
+  gotoBackpage(){
+    // ?isUpdate=true'
+    this.router.navigate(['/start-match']);
   }
 }
