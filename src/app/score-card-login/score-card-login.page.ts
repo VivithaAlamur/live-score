b45379c51730e1a15fa41fa256c945fcc70eff24
Vivitha Alamur <vivitha.alamur@tadigital.com>
2021-06-13 12:12:12 +0530

    Added the validations for the form

--- src/app/score-card-login/score-card-login.page.ts
+++ src/app/score-card-login/score-card-login.page.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../services/login.service';
 
@@ -12,7 +12,7 @@ import { LoginService } from '../services/login.service';
 export class ScoreCardLoginPage implements OnInit {
 
   loginForm: FormGroup;
-
+  submitted=false;
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
@@ -24,16 +24,23 @@ export class ScoreCardLoginPage implements OnInit {
   }
   initForm() {
     this.loginForm = this.formBuilder.group({
-      Emailid: null,
-      Password: null
+      Emailid: [null, [Validators.required]],
+      Password: [null, [Validators.required]]
     })
   }
   login(){
+  this.submitted=true;
+    if (this.loginForm.invalid) {
+      return;
+  }else{
     const obj= this.loginForm.value;
     this.loginService.loginForMatches(obj).subscribe(async data => {
       this.loginForm.reset();
       this.router.navigate(['/matches-list'])
     });
+    this.submitted=false;
   }
+}
+  get f() { return this.loginForm.controls; }
 
 }
